5c43d1c6f65c1a5d0fde2682315a137d3cb14488
SashaRoberts <45043185+kittensighting@users.noreply.github.com>
2020-07-10 11:32:59 -0500

    basic solutions and studydrills

---
+++ Learn-Python-3-The-Hard-Way/ex3-studydrills.py
@@ -0,0 +1,42 @@
+# print text
+print("I will now count my chickens:")
+# 30 divided by 6 plus 25
+print("Hens", 25 + 30 / 6)
+# 25*3 = 75
+# 75/4 = 18 r 3
+# 100 - 3 = 97
+print("Roosters", 100 - 25 * 3  % 4)
+
+# print text
+print("Now I will count the eggs:")
+
+# 4%2 = 0
+# 1.0/4 = 0.25
+# add and subtract left to right
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# 5 < -2 which is FALSE
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# result
+print(3 + 2 < 5 - 7)
+
+# print text, result
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+# print text
+print("Oh, that's why it's False.")
+
+# print text
+print("How about some more.")
+
+# print text, results
+("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal", 5 <= -2)
+
+print("5/4 =", 5/4) # in python2 this would be truncated
+print("5.0/4 =", 5.0/4)
+print("5/4.0 =",5/4.0)
+print("5.0/4.0 =", 5.0/4.0)
